e0b2f9d59f557cd749533aa0567e027d84b32ed3
dboicourt <dboicourt@gmail.com>
2018-08-25 11:27:16 -0700

    testing again

---
+++ keyboardArrToDisplay.js
@@ -0,0 +1,9 @@
+//sentance variable is the input from the working array converted into a string, should already be defined
+function capsLock() {
+    if (capsCheck === true) {
+        
+        return (displayString.toUpperCase())
+    } else {
+        return displayString
+    }
+}
