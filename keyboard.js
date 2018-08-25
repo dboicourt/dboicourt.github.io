9c2fe302d32b367e35ad341a13eecbe84bf93079
dboicourt <dboicourt@gmail.com>
2018-08-25 13:20:05 -0700

    compiled versions

---
+++ keyboard.js
@@ -0,0 +1,374 @@
+let text = document.getElementById("text");
+let buttonA = document.getElementById("buttonA");
+let buttonB = document.getElementById("buttonB");
+let buttonC = document.getElementById("buttonC");
+let buttonD = document.getElementById("buttonD");
+let buttonE = document.getElementById("buttonE");
+let buttonF = document.getElementById("buttonF");
+let buttonG = document.getElementById("buttonG");
+let buttonH = document.getElementById("buttonH");
+let buttonI = document.getElementById("buttonI");
+let buttonJ = document.getElementById("buttonJ");
+let buttonK = document.getElementById("buttonK");
+let buttonL = document.getElementById("buttonL");
+let buttonM = document.getElementById("buttonM");
+let buttonN = document.getElementById("buttonN");
+let buttonO = document.getElementById("buttonO");
+let buttonP = document.getElementById("buttonP");
+let buttonQ = document.getElementById("buttonQ");
+let buttonR = document.getElementById("buttonR");
+let buttonS = document.getElementById("buttonS");
+let buttonT = document.getElementById("buttonT");
+let buttonU = document.getElementById("buttonU");
+let buttonV = document.getElementById("buttonV");
+let buttonW = document.getElementById("buttonW");
+let buttonX = document.getElementById("buttonX");
+let buttonY = document.getElementById("buttonY");
+let buttonZ = document.getElementById("buttonZ");
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+let button4 = document.getElementById("button4");
+let button5 = document.getElementById("button5");
+let button6 = document.getElementById("button6");
+let button7 = document.getElementById("button7");
+let button8 = document.getElementById("button8");
+let button9 = document.getElementById("button9");
+let button0 = document.getElementById("button0");
+let buttonCom = document.getElementById("button,");
+let buttonPer = document.getElementById("button.");
+let buttonSemi = document.getElementById("button;");
+let buttonApo = document.getElementById("button'");
+let buttonEqu = document.getElementById("button=");
+let buttonDsh = document.getElementById("button-");
+let buttonForSla = document.getElementById("button/");
+let buttonCapLck = document.getElementById("buttonCapLck");
+let del = document.getElementById("buttonDel");
+let spc = document.getElementById("buttonSpc");
+let displayText = [];
+if(text.value === ''){
+    text.placeholder = "Your Text Here";
+}
+function addA(){
+    displayText.push("a");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonA.addEventListener('click',addA);
+function addB(){
+    displayText.push("b");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonB.addEventListener('click',addB);
+function addC(){
+    displayText.push("c");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonC.addEventListener('click',addC);
+function addD(){
+    displayText.push("d");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonD.addEventListener('click',addD);
+function addE(){
+    displayText.push("e");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonE.addEventListener('click',addE);
+function addF(){
+    displayText.push("f");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonF.addEventListener('click',addF);
+function addG(){
+    displayText.push("g");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonG.addEventListener('click',addG);
+function addH(){
+    displayText.push("h");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonH.addEventListener('click',addH);
+function addI(){
+    displayText.push("i");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonI.addEventListener('click',addI);
+function addJ(){
+    displayText.push("j");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonJ.addEventListener('click',addJ);
+function addK(){
+    displayText.push("k");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonK.addEventListener('click',addK);
+function addL(){
+    displayText.push("l");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonL.addEventListener('click',addL);
+function addM(){
+    displayText.push("m");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonM.addEventListener('click',addM);
+function addN(){
+    displayText.push("n");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonN.addEventListener('click',addN);
+function addO(){
+    displayText.push("o");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonO.addEventListener('click',addO);
+function addP(){
+    displayText.push("p");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonP.addEventListener('click',addP);
+function addQ(){
+    displayText.push("q");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonQ.addEventListener('click',addQ);
+function addR(){
+    displayText.push("r");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonR.addEventListener('click',addR);
+function addS(){
+    displayText.push("s");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonS.addEventListener('click',addS);
+function addT(){
+    displayText.push("t");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonT.addEventListener('click',addT);
+function addU(){
+    displayText.push("u");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonU.addEventListener('click',addU);
+function addV(){
+    displayText.push("v");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonV.addEventListener('click',addV);
+function addW(){
+    displayText.push("w");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonW.addEventListener('click',addW);
+function addX(){
+    displayText.push("x");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonX.addEventListener('click',addX);
+function addY(){
+    displayText.push("y");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonY.addEventListener('click',addY);
+function addZ(){
+    displayText.push("z");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonZ.addEventListener('click',addZ);
+function add1 (){
+    displayText.push("1");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+button1.addEventListener('click',add1);
+function add2 (){
+    displayText.push("2");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+button2.addEventListener('click',add2);
+function add3 (){
+    displayText.push("3");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+button3.addEventListener('click',add3);
+function add4 (){
+    displayText.push("4");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+button4.addEventListener('click',add4);
+function add5 (){
+    displayText.push("5");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+button5.addEventListener('click',add5);
+function add6 (){
+    displayText.push("6");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+button6.addEventListener('click',add6);
+function add7 (){
+    displayText.push("7");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+button7.addEventListener('click',add7);
+function add8 (){
+    displayText.push("8");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+button8.addEventListener('click',add8);
+function add9 (){
+    displayText.push("9");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+button9.addEventListener('click',add9);
+function add0 (){
+    displayText.push("0");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+button0.addEventListener('click',add0);
+function delText(){
+    displayText.pop();
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+del.addEventListener('click',delText);
+function addSpc(){
+    displayText.push(" ");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+spc.addEventListener('click',addSpc);
+function addPer(){
+    displayText.push(".");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonPer.addEventListener('click',addPer);
+function addCom(){
+    displayText.push(",");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonCom.addEventListener('click',addCom);
+function addForSla(){
+    displayText.push("/");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonForSla.addEventListener('click',addForSla);
+function addSemi(){
+    displayText.push(";");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonSemi.addEventListener('click',addSemi);
+function addApo(){
+    displayText.push("'");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonApo.addEventListener('click',addApo);
+function addEqu(){
+    displayText.push("=");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonEqu.addEventListener('click',addEqu);
+function addDsh(){
+    displayText.push("-");
+    let result = displayText.join('');
+    text.placeholder = result;
+    console.log(result);
+}
+buttonDsh.addEventListener('click',addDsh);
+function capitalize(){
+    if(buttonCapLck.value === true)
+    (buttonCapLck.value = false)
+    else if(buttonCapLck.value === false)
+    (buttonCapLck.value = true);
+    console.log(buttonCapLck.value);
+}
+buttonCapLck.addEventListener('click',capitalize);
